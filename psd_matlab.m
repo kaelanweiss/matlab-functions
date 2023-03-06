3425abb88b7a4bbb8efd1cd37e0be0d0e83a8e65
kaelan weiss <weisskae@oregonstate.edu>
2023-03-06 15:20:58 -0800

    added psd_matlab.m

---
+++ psd_matlab.m
@@ -0,0 +1,20 @@
+function [P,f] = psd_matlab(X,fs)
+% 
+%
+
+% size
+nt = size(X,1);
+n = 2^nextpow2(nt);
+
+% hanning window
+H = repmat(hann(nt),[1 size(X,2)]);
+
+% DFFT
+Y = fft(H.*X);
+f = fs*(0:(n/2))/nt;
+
+% one-sided PSD
+P2 = abs(Y/n);
+P = P2(1:n/2+1,:);
+P(2:end-1,:) = 2*P(2:end-1,:);
+
