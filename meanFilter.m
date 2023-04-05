97837afc135999a337560c7b96d9899ac3c07f5a
kaelan weiss <weisskae@oregonstate.edu>
2023-04-04 19:03:38 -0700

    added "keepends" functionality to meanFilter

--- meanFilter.m
+++ meanFilter.m
@@ -8,6 +8,7 @@ function out = meanFilter(in,k,varargin)
 %   in: vector to be filtered
 %   k: width of window
 %   nanflag: (optional) 'omitnan' to ignore nans in mean calculation
+%   endpoints: (optional) 'keepends' to ignore nans in mean calculation
 %
 % Output
 %   out: filtered vector
@@ -16,17 +17,26 @@ N = length(in);
 nan_cutoff = 0.5;
 out = nan(size(in));
 
+% parse inputs
 omitnan = false;
-if nargin == 3 && strcmpi(varargin{1},'omitnan')
+if nargin > 2 && any(strcmpi(varargin,'omitnan'))
     omitnan = true;
 end
 
+keepends = false;
+if nargin > 2 && any(strcmpi(varargin,'keepends'))
+    keepends = true;
+end
+
+% make sure k is okay for the given input vector
 assert(mod(k,2)==1,'k must be odd');
 assert(N>2*k,'k too large compared to N');
 
-for i = ((k+1)/2) : N-((k-1)/2)
-    idx = [-1 1]*(k-1)/2 + i;
-    slicei = in(idx(1):idx(2));
+% main loop, this leaves endpoints as nans
+for i = 1:N
+    idx = (-(k-1)/2:(k-1)/2) + i;
+    idx = intersect(1:N,idx);
+    slicei = in(idx);
     nNan = length(find(isnan(slicei)));
     if nNan/k < nan_cutoff
         if omitnan
@@ -37,4 +47,10 @@ for i = ((k+1)/2) : N-((k-1)/2)
     else
         out(i) = NaN;
     end
+end
+
+% nan out ends if so desired (default)
+if ~keepends
+    idx = [1:(k-1)/2 N-(k-1)/2+1:N];
+    out(idx) = nan;
 end
